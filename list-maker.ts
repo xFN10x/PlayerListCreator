cc94907d3720dec8c88c32e3ba1ddd87c612b28b
xFN10x <mathdudex09@gmail.com>
2026-02-19 11:54:53 -0400

    now do folder checking

--- list-maker.ts
+++ list-maker.ts
@@ -1,18 +1,36 @@
 import nbt from "prismarine-nbt";
 import readline from "node:readline";
 import fs from "node:fs";
+import { exit } from "node:process";
 
 const rl = readline.createInterface(process.stdin, process.stdout);
 
-console.log("test");
+console.log("Player List Maker by xFN10x");
+
+function resolve(baseString: string, ...paths: string[]): string {
+  const parsedBase = baseString.replaceAll("\\", "/");
+  const parsedPaths = paths.join("/");
+
+  return `${parsedBase}${parsedBase.endsWith("/") ? "" : "/"}${paths}`;
+}
 
 function start() {
-  rl.question("Minecraft world folder: ", (folder) => {
+  rl.question("Minecraft world folder (type 'exit' to exit): ", (folder) => {
+    if (folder == "exit") {
+      console.log("Bye!");
+      exit();
+    }
     try {
       if (fs.existsSync(folder)) {
-    
+        const playerDataPath = resolve(folder, "playerdata");
+        if (fs.existsSync(playerDataPath)) {
+        } else {
+          console.log("Not a Minecraft world!");
+          start();
+          return;
+        }
       } else {
-        console.log("That folder doesn't exist!")
+        console.log("That folder doesn't exist!");
         start();
         return;
       }
@@ -24,4 +42,4 @@ function start() {
   });
 }
 
-start()
+start();
